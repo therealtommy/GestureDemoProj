0908999e9ab8b71ccc0cb9a7438172b23441bd81
therealtommy <sokolovai1887@ya.ru>
2025-12-15 15:03:01 +0700

    TapPressDemo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.foundation.layout)
+    implementation(libs.androidx.glance)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -6,8 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
@@ -19,8 +22,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceModifier
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -39,7 +46,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    ClickDemo(modifier)
+    TapPressDemo(modifier)
 }
 
 @Preview(showBackground = true)
@@ -50,24 +57,32 @@ fun GreetingPreview() {
     }
 }
 @Composable
-fun ClickDemo(modifier: Modifier = Modifier) {
-    var colorState by remember { mutableStateOf(true)}
-    var bgColor by remember { mutableStateOf(Color.Blue) }
+fun TapPressDemo(modifier: Modifier = Modifier) {
 
-    val clickHandler = {
+    var textState by remember {mutableStateOf("Waiting ....")}
 
-        colorState = !colorState
-        bgColor = if (colorState) {
-            Color.Blue
-        } else {
-            Color.DarkGray
-        }
+    val tapHandler = { status : String ->
+        textState = status
     }
-
-    Box(
-        modifier
-            .clickable { clickHandler() }
-            .background(bgColor)
-            .size(100.dp)
-    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize() as GlanceModifier
+    ) {
+        Box(
+            Modifier
+                .padding(10.dp)
+                .background(Color.Blue)
+                .size(100.dp)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = { tapHandler("onPress Detected") },
+                        onDoubleTap = { tapHandler("onDoubleTap Detected") },
+                        onLongPress = { tapHandler("onLongPress Detected") },
+                        onTap = { tapHandler("onTap Detected") }
+                    )
+                }
+        )
+        Spacer(Modifier.height(10.dp))
+        Text(textState)
+    }
 }
