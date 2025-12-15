50f1cef376bf188de86acf460aaa60b3a93b0e3a
therealtommy <sokolovai1887@ya.ru>
2025-12-15 14:58:10 +0700

    ClickDemo fix

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gesturedemo.ui.theme.GestureDemoTheme
 
 class MainActivity : ComponentActivity() {
